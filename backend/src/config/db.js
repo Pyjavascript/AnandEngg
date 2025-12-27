cdc18a60ae708a2906a1055f5f13f7916b29ebd3
Manpreet Singh <ms0014460@gmail.com>
2025-12-27 06:56:58 +0530

    changes in env and mysql

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,18 @@ const mysql = require('mysql2');
 
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || DB_PASS,
+  database: process.env.MYSQLDATABASE || DB_NAME,
+  port: process.env.MYSQLPORT || 3306;
+});
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL Database");
 });
 
 module.exports = db;
