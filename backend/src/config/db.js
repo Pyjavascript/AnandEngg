0b575b2c5ca84849dff7bdd58c59d15e51c5285c
Manpreet Singh <ms0014460@gmail.com>
2026-08-28 10:30:35 +0530

    changes in code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,27 @@
-// const mysql = require('mysql2');
+// use ananddb;
+
+// SHOW tables from ananddb;
+
+// SELECT * FROM app_notifications;
+// SELECT * FROM report_categories;
+// SELECT * FROM report_parts;
+// SELECT * FROM report_submissions;
+// SELECT * FROM report_templates;
+// SELECT * FROM reports;
+// SELECT * FROM roles;
+// SELECT * FROM submission_values;
+// SELECT * FROM template_fields;
+// SELECT * FROM user_push_tokens;
+// SELECT * FROM users;
+
+
+// module.exports = db;
+// const mysql = require('mysql2/promise');
 
 // const db = mysql.createPool({
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
+//   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_NAME,
 //   port: process.env.DB_PORT,
 //   waitForConnections: true,
@@ -12,14 +30,16 @@
 // });
 
 // module.exports = db;
+
+
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
